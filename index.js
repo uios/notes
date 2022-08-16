525e9fd5a17c3cf0af5bf26ad71c3c5919829776
thebanon <91961816+thebanon@users.noreply.github.com>
2022-08-16 18:37:13 -0400

    Update Shell UI

--- index.js
+++ index.js
@@ -11,85 +11,8 @@ window.onload = ()=>{
 
 function init() {
     dom.body.dataset.load = "ed";
-
-    touch.events = {
-        dbltap: on.touch.dbltap,
-        drag: on.touch.drag,
-        press: on.touch.press,
-        tap: on.touch.tap
-    };    
-    touch.ing = false;
     
     dom.body.dataset.theme = "meridiem";
-    dom.body.addEventListener("click", function(e) {
-        if(window.touch.ing === false) {
-            on.touch.tap(e);
-            //console.log(e.type,window.touch.ing);
-        } else {
-            window.touch.ing = false;
-            //console.log(e.type,window.touch.ing);
-        }
-    });
-    dom.body.addEventListener("touchstart", function(e) {
-        window.touch.ing = true;
-        touch.handler(event);
-        console.log(e.type);
-    }, {
-        passive: true
-    });
-    dom.body.addEventListener("touchmove", touch.handler, {
-        passive: true
-    });
-    dom.body.addEventListener("touchcancel", touch.handler, false);
-    dom.body.addEventListener("touchend", function(e) {
-        //window.touch.ing = false;
-        touch.handler(event);
-        console.log(e.type);
-    });
     
     window.location.pathname.router();
 }
-
-window.mouseover = {
-    menu: {
-        item: target => {
-            byId('menu-backdrop') ? byId('menu-backdrop').remove() : null;
-            var html = `<picture class="-hover -tap-display-flex -tilde--hover--tap-display-flex display-none height-100pct left-0 position-fixed top-0 width-100pct z-index-1" data-tap="event.target.closest('header').classList.remove('-tap')" id="menu-backdrop"></picture>`;
-            target.closest('box').insertAdjacentHTML('afterend',html);            
-        }
-    }
-}
-
-window.mouseout = {
-    menu: {
-        item: target => {
-            //$(event.target.closest('header')).toggleClass('-hover').toggleClass('-tap'); onhover.menu.item(event.target);
-        }
-    }
-}
-
-window.tap = {
-    menu: {
-        item: target => {
-            $(target.closest('header')).toggleClass('-hover').toggleClass('-tap'); 
-            mouseover.menu.item(target);
-        }
-    }
-}
-
-window.on.submit = {};
-
-window.on.submit.index = {};
-window.on.submit.index.path = function(event) {
-    console.log(event);
-    event.preventDefault();
-    const form = event.target;
-    const path = form.find('input[type="text"]');
-    var GOT = path.value.split("\\");
-    GOT.shift();
-    var href = rout.ed.url(GOT.filter(n => n));
-    console.log(GOT,href);
-    href.router();
-}
-
-window.submit = window.on.submit;
