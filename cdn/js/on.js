df4a3f61c1ddb9f8e77637b463aaaffc29c3e7bf
Anon ||| Mous <3849997+anoniiimous@users.noreply.github.com>
2022-08-16 21:14:57 -0400

    Debug Note UX

--- cdn/js/on.js
+++ cdn/js/on.js
@@ -7,5 +7,6 @@ window.on.key = {};
 window.on.input = {};
 window.on.input.note = function(textarea) {
     console.log(textarea.scrollHeight);
+    textarea.style.height = 0;
     textarea.style.height = textarea.scrollHeight + 'px';
 }
